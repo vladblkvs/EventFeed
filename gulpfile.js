da20655bb8da6bdd18fd5a97f4fcf0e36df4cc16
vladblkvs <gremlinux@gmail.com>
2020-05-09 23:36:31 +0300

    Add feed layout

--- gulpfile.js
+++ gulpfile.js
@@ -67,8 +67,8 @@ gulp.task("webp", function() {
 
 gulp.task("copy", function() {
   return gulp.src([
-      "source/manifest/site.webmanifest",
-      "source/fonts/**/*.{woff,woff2}",
+      // "source/manifest/site.webmanifest",
+      // "source/fonts/**/*.{woff,woff2}",
       "source/img/**",
       "!source/img/sprite/**"
     ], {
@@ -101,5 +101,5 @@ gulp.task("refresh", function(done) {
   done();
 });
 
-gulp.task("build", gulp.series(/*"clean","copy", "sprite", */"html"/*, "css", "js"*/));
+gulp.task("build", gulp.series(/*"clean",*/"copy", /*"sprite", */"html"/*, "css", "js"*/));
 gulp.task("start", gulp.series("build", "server"));
